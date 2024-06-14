ca3abb70d9930de2150c1d93424c7978736dcb96
bohyeong <tjqhgud9104@naver.com>
2024-06-14 15:19:17 +0900

    first distribution

---
+++ api/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) => res.send('Express on Vercel'))
+app.listen(3100, ()=> console.log('Server ready on port 3100.'))
+
+module.exports = app
