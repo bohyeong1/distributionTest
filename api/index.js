7e879f78ae396b9aac8f1fd9f11117ac07489978
bohyeong <tjqhgud9104@naver.com>
2024-06-18 09:51:37 +0900

    distribution test

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req,res) => res.send('Express on Vercel'))
+app.get('/', (req,res) => res.send('Express on Vercel 배포 성공~!'))
 app.listen(3100, ()=> console.log('Server ready on port 3100.'))
 
 module.exports = app
